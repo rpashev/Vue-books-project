ad9c89186ffd2a2824532d70bec7837ddcd3002b
rpashev <56789393+rpashev@users.noreply.github.com>
2020-03-28 12:47:46 +0200

    Initial commit
    Initial structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appHome: Home
+  }
+}
+</script>
+
+<style>
+
+</style>
