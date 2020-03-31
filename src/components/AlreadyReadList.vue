86cf3e6762a8f5305aadd4a9eed1ff375af7750b
rpashev <56789393+rpashev@users.noreply.github.com>
2020-03-31 16:06:15 +0300

    Listing user-lists

--- src/components/AlreadyReadList.vue
+++ src/components/AlreadyReadList.vue
@@ -1,5 +1,67 @@
 <template>
-    <div>
-        Already read list
-    </div>
+  <div class="booklist">
+    <app-book-card v-for="book in books" :key="book.id" :book="book"></app-book-card>
+    <div v-if="!this.isLoaded">Loading...</div>
+  </div>
 </template>
+
+<script>
+import Card from "./Bookcard";
+import db from "../firebase-config";
+
+export default {
+  components: {
+    appBookCard: Card
+  },
+  data() {
+    return {
+      books: [],
+      bookIDs: [],
+      userID: localStorage.getItem("userID")
+    };
+  },
+  created() {
+    db.collection("userData")
+      .doc(this.userID)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          doc.data().alreadyRead.forEach(id => this.bookIDs.unshift(id));
+        } else {
+          alert("No such document!");
+        }
+      })
+      .catch(function(error) {
+        alert("Error getting document:", error);
+      });
+
+    console.log(this.bookIDs);
+    //////////////////getting the books
+    db.collection("books")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          if (this.bookIDs.includes(doc.id)) {
+            let book = { ...doc.data().book, id: doc.id };
+            this.books.unshift(book);
+          }
+        });
+      })
+      .catch(err => console.log(err));
+  },
+
+  computed: {
+    isLoaded() {
+      return !!this.books.length;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.booklist {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
